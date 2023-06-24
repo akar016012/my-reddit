167ed68c4c2d3bc2559d78de1362fd736b3f94f9
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-06-24 18:54:58 -0500

    added supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pjcfoqaqvtguvgtryxrm.supabase.co";
+const SUPABASE_KEY = toString(SUPABASE_KEY);
 const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
