f4410463c046a268e9da921cee37ed4ffdcfcb5a
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-08-29 08:10:05 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,6 +5,5 @@ const supabaseUrl = "https://pjcfoqaqvtguvgtryxrm.supabase.co";
 
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 export const supabase = createClient(
-  supabaseUrl,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqY2ZvcWFxdnRndXZndHJ5eHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2MDYwMzcsImV4cCI6MjAwMzE4MjAzN30.6W9OPh1euJxUwwjthAGNQnOM8LNW_nRAro8dThnjO8w"
+  supabaseUrl,supabaseKey
 );
