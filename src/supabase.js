ddfb9fb09a0c3a1c3567113c362f6b1ffd79a236
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-08-29 08:23:26 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pjcfoqaqvtguvgtryxrm.supabase.co";
-
+//grabbing the api key from environmnet variables
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 export const supabase = createClient(
   supabaseUrl,supabaseKey
